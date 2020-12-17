10e0288dbdd19723c8bc49b50475622bae8befa4
WiranataK <wiranatak99@gmail.com>
2020-12-17 18:49:09 +0900

    Create signupinvalid2.js

---
+++ shared-objects/signupinvalid2.js
@@ -0,0 +1,8 @@
+module.exports = {
+    signupURL: "http://ocatelkom.co.id/register",
+    firstname: "",
+    lastname: "dummy",
+    email: "dummy@gmail.com",
+    phonenumber: "080808080808",
+    password: "qywter162534"
+}
