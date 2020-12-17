0048d6b2317d924d7887bfcd56db1c8a602000fe
WiranataK <wiranatak99@gmail.com>
2020-12-17 18:49:32 +0900

    Create signupinvalid8-steps.js

---
+++ step-definitions/signupinvalid8-steps.js
@@ -0,0 +1,50 @@
+module.exports = function () {
+    
+    this.Given(/^User browse register page Failed Version 8$/, function () {
+        helpers.loadPage(shared.signupobjects.signupURL);
+    });
+
+    this.Given(/^User fill first name to register Failed Version 8$/, function () {
+        driver.then(function (){
+            return page.signupinvalid8.performFillFN();
+        })
+    });
+
+    this.Given(/^User fill last name to register Failed Version 8$/, function () {
+        driver.then(function (){
+            return page.signupinvalid8.performFillLN();
+        })
+    });
+
+    this.Given(/^User fill active email to register Failed Version 8$/, function () {
+        driver.then(function (){
+            return page.signupinvalid8.performFillE();
+        })
+    });
+
+    this.Given(/^User fill active phone number to register Failed Version 8$/, function () {
+        driver.then(function (){
+            return page.signupinvalid8.performFillPN();
+        })
+    });
+
+    this.Given(/^User fill password to register Failed Version 8$/, function () {
+        driver.then(function (){
+            return page.signupinvalid8.performFillP();
+        })
+    });
+
+    this.When(/^User click button create account Failed Version 8$/, function () {
+        driver.then(function (){
+            return page.ClickSignUpButton.perform();
+        })
+    });
+
+    this.Then(/^User get alert 8$/, function () {
+        driver.wait(function() {
+            return driver.getTitle().then(function(title) {
+              return title === 'Registered - Telkom OCA';
+            });
+        }, 10000);
+    })
+}
